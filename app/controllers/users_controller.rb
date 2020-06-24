5e58113b124a0460b6a77c5854e09c73ec9f81d7
Pat McClernan <patmccler@gmail.com>
2020-06-24 12:03:09 -0400

    worked through some feature tests, currently on session#new

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -5,6 +5,20 @@ class UsersController < ApplicationController
   end
 
   def create
-    
+    user = User.create(user_params)
+    session[:user_id] = user.id
+
+    redirect_to user_path(user)
   end
+
+  def show
+    @user = User.find(params[:id])
+  end
+
+private
+
+  def user_params
+    params.require(:user).permit(:name, :height, :happiness, :nausea, :tickets, :password)
+  end
+
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,9 @@
 Rails.application.routes.draw do
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
   root "static#welcome"
-  resources :users, only: [:new, :create]
+  resources :users, only: [:new, :create, :show]
+
+  get "/signin", to: "sessions#new"
+  post "/signin", to: "sessions#create"
 
 end
